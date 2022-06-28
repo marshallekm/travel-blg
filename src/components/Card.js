b41f3beb82fc97eb878aeba365ab9043964166fe
Erika Kawashima <marshallekm@gmail.com>
2022-06-28 19:52:23 +0900

    classes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Card(props){
   return(
-    <div>
-    <h1>{props.item.title}</h1>
-    <h2>{props.item.location}</h2>
-    <h2>{props.item.googleMapsUrl}</h2>
-    <h3>{props.item.startDate}</h3>
-    <h3>{props.item.endDate}</h3>
-    <h3> {props.item.description}</h3>
-    <h3> <img src= {props.item.imageUrl}></img></h3>
+    <div className="card">
+    <h1 className="card--title">{props.item.title}</h1>
+    <h2 className="card--location">{props.item.location}</h2>
+    <h2 className="card--link"><a href ='{props.item.googleMapsUrl}'>"View in Google Maps"</a></h2>
+    <h3 className="card--start">{props.item.startDate}</h3>
+    <h3 className="card--end">{props.item.endDate}</h3>
+    <h3 className="card--description"> {props.item.description}</h3>
+    <h3 className="card--img"> <img src= {props.item.imageUrl}></img></h3>
     </div>
   )
 }
