8f378fd90bde2c4a4929a3365031928849455b1c
Erika Kawashima <marshallekm@gmail.com>
2022-07-06 19:54:36 +0900

    query

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import funFact from '../images/fun-fact.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMap } from '@fortawesome/free-regular-svg-icons'
 import {faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 // import MediaQuery from 'react-responsive'
 
 export default function Card(props){
@@ -11,11 +13,13 @@ export default function Card(props){
         <img src= {props.item.imageUrl} className= "card--img" alt= {props.item.imageUrl}></img>
     </div>
     <div className="card--info">
+        <div className="card--links">
+          <h2 className="card--location"><FontAwesomeIcon icon={faLocationDot} className="location-dot" />{props.item.location}</h2>
+          <h2 className="card--link"><a href ={props.item.googleMapsUrl}>View in Google Maps <FontAwesomeIcon icon={faMap} /></a></h2>
+        </div>
       <h1 className="card--title">{props.item.title}</h1>
-        <h2 className="card--location"><FontAwesomeIcon icon={faLocationDot} className="location-dot" />{props.item.location}</h2>
-        <h2 className="card--link"><a href ={props.item.googleMapsUrl}>View in Google Maps <FontAwesomeIcon icon={faMap} /></a></h2>
-        <h3 className="card--start">{props.item.startDate}</h3>
-        <h3 className="card--description"> {props.item.description}</h3>
+      <h3 className="card--start"> Date: {props.item.startDate}</h3>
+      <h3 className="card--description"> <img className= "fun--fact" src={funFact} alt=""/> {props.item.description}</h3>
     </div>
   </div>
   )
--- src/data.js
+++ src/data.js
@@ -5,14 +5,14 @@ export default [
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "July, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, as well as one of Japan's most well known cultural icons.",
+        description: "Mount Fuji is the tallest mountain in Japan, as well as one of it's most well known cultural/visual icons.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     }
       ,
     {
        title: "Boudhanath Stupa",
         location: "Nepal",
-        googleMapsUrl: "https://www.google.com/maps/place/Boudhanath+Stupa/@27.7215109,85.3598034,17z/data=!3m1!4b1!4m5!3m4!1s0x39eb1bda4a951f0f:0x3ddabb234891c3bd!8m2!3d27.7215062!4d85.3619974https://www.google.com/maps/place/Boudhanath+Stupa/@27.7215109,85.3598034,17z/data=!3m1!4b1!4m5!3m4!1s0x39eb1bda4a951f0f:0x3ddabb234891c3bd!8m2!3d27.7215062!4d85.3619974",
+        googleMapsUrl: "https://www.google.com/maps/place/Boudhanath+Stupa/@27.7215109,85.3598034,17z/data=!4m8!1m2!3m1!2sBoudhanath+Stupa!3m4!1s0x39eb1bda4a951f0f:0x3ddabb234891c3bd!8m2!3d27.7215064!4d85.3619973",
         startDate: "Dec, 2019",
         description: "Ancient Buddhist place of worship, nestled within the hustle and bustle of Kathmandu.",
         imageUrl: "https://images.unsplash.com/photo-1648702978569-d21e6c4701b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
